53779c7e98668d7c35cc4f6f2fd5eb343089b4af
Vit 42 <v.v.safronov@gmail.com>
2021-01-27 14:08:12 +0100

    Initial Commit

---
+++ Tip + bill split (portfolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip + bill split (portfolio
+//
+//  Created by Vitalii Safronov on 2021. 01. 27..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
